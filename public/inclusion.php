103a08c8414ec723982d67e7c6253e88b01ce23a
bryan-dev-maker <usaibryan00@gmail.com>
2025-12-05 03:30:25 +0100

    pageInclusion

---
+++ public/inclusion.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Durabilite</title>
+    <link rel="stylesheet" href="./css/Durabilite.css">
+</head>
+<body>
+    <nav>
+        <div class="logo_container">
+             <img src="./image/logo.png">
+        </div>
+        <div class="nav_links_container">
+            <a href="#">Accueil</a>
+            <a href="#">Inclusion</a>
+            <a href="#">Responsabilité</a>
+            <a href="#">Durabilité</a>
+            <a href="#">Inscription</a>
+        </div>
+        <div class="nav_log_container">
+            <button>Se déconnecter</button>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class ="middle container">
+    
+            <div class="paragrphe1">
+                <h1>L’inclusion numérique, dans la démarche NIRD, c’est faire en sorte que tout le monde puisse accéder, comprendre et utiliser le numérique, quels que soient les moyens financiers, le matériel ou le niveau de compétence.</h1>
+                <p>
+Réduire les barrières d’accès
+L’usage de logiciels libres et open source permet d’équiper des élèves et des établissements sans coût de licence, à l’école comme à la maison. Un ordinateur ancien reconditionné avec Linux et des outils libres devient un vrai poste de travail, ce qui limite la fracture entre ceux qui ont du matériel récent et ceux qui n’en ont pas.​</p>
+            </div>
+            <div class="container center">
+            <div class="image">
+                <img src="./image/inclusion.jpg" alt="Inclusion">
+            </div>
+            <div class="paragraphe2">
+                <p>Adapter les outils aux besoins de chacun
+Les logiciels open source offrent souvent des possibilités de personnalisation et d’accessibilité : synthèse vocale, aides à la lecture et à l’écriture, interfaces simplifiées, paramétrages adaptés aux troubles de l’apprentissage. L’école peut choisir, tester, ajuster ces outils pour répondre aux besoins spécifiques des élèves, plutôt que subir un environnement figé.​</p>
+            </div>
+            </div>
+            <div CLASS="paragraphe3">
+                <p>Donner du pouvoir d’agir
+L’inclusion numérique, c’est aussi donner aux élèves, aux enseignants et aux familles la capacité de comprendre et de maîtriser leurs outils. En travaillant avec des logiciels libres, en expliquant comment ils fonctionnent et pourquoi on les choisit, on propose un numérique où chacun peut apprendre, expérimenter et ne pas se sentir exclu parce qu’il ne possède pas « la bonne marque » ou « le dernier abonnement ».</p>
+            </div>
+        </div>
+        <div class="game">
+        </div>
+
+    </div>
+</body>
+</html>
