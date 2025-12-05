923d3f45d73e1c355939397e8e4f537697d72a00
LabroyJulie <julie24.labroy04@gmail.com>
2025-12-05 03:18:44 +0100

    page responsabilite html css

---
+++ public/responsabilite.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/responsabilite.css">
+</head>
+<body>
+<nav>
+        <div class="logo_container">
+             <img src="./image/logo.png">
+        </div>
+        <div class="nav_links_container">
+            <a href="#">Accueil</a>
+            <a href="#">Inclusion</a>
+            <a href="#">Responsabilité</a>
+            <a href="#">Durabilité</a>
+            <a href="#">Inscription</a>
+            <a href="#">Connexion</a>
+        </div>
+        <div class="nav_log_container">
+            <p>Se déconnecter</p>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="paragraphe1">
+            <p>Responsabilité pédagogique et citoyenne
+            Être responsable, c’est aussi faire du numérique un objet d’éducation, pas seulement un outil. Avec NIRD, les choix techniques (Linux, logiciels libres, reconditionnement) deviennent des supports pour parler de droits numériques, de coopération, de transparence du code, de modèles économiques. Les élèves ne sont plus seulement utilisateurs, mais apprennent à comprendre, questionner et parfois contribuer.</p>
+        </div>
+        <div class="midlle_container">
+            <div class="image">
+            <img src="./images/fracture.png" alt="illustration de la fracture numérique">
+            </div>
+            <div class="paragraphe2">
+                <p>Responsabilité écologique et budgétaire
+                La responsabilité informatique implique enfin de tenir compte des ressources : ne pas gaspiller le matériel, ne pas acheter du neuf sans nécessité, optimiser ce qui existe déjà. Reconditionner, migrer vers des systèmes plus sobres, mutualiser des solutions libres, ce sont des actes concrets qui alignent le numérique de l’établissement avec ses engagements de service public et de développement durable.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
