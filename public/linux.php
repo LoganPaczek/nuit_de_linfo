1e61974f8fa959a654ed6cc987b9372d12e75210
LabroyJulie <julie24.labroy04@gmail.com>
2025-12-05 01:20:57 +0100

    start linux page

---
+++ public/linux.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/acceuil.css">
+    <link rel="stylesheet" href="css/linux.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <div class="logo_container">
+             <img src="./image/logo.png">
+        </div>
+        <div class="nav_links_container">
+            <a href="#">Accueil</a>
+            <a href="#">Inclusion</a>
+            <a href="#">Responsabilité</a>
+            <a href="#">Durabilité</a>
+            <a href="#">Inscription</a>
+            <a href="#">Connexion</a>
+        </div>
+        <div class="nav_log_container">
+            <p>Se connecter</p>
+            <p>Se déconnecter</p>
+        </div>
+    </nav>
+
+
+</body>
+</html>
